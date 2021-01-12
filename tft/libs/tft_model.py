3bc1eba51ad817394ebb16b5cdca6aefb8c1def3
Patryk Dybczak <patryk.dybczak@relia-sol.pl>
2021-01-12 08:11:02 +0100

    comment out printing model summary

--- tft/libs/tft_model.py
+++ tft/libs/tft_model.py
@@ -1052,7 +1052,7 @@ class TemporalFusionTransformer(object):
 
       model = tf.keras.Model(inputs=all_inputs, outputs=outputs)
 
-      print(model.summary())
+#       print(model.summary())
 
       valid_quantiles = self.quantiles
       output_size = self.output_size
